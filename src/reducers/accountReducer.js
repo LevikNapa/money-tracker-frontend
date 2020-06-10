9c1b15556eb81aaf2dfbf1734e6f0e0d21ee2f18
github username <levik010@gmail.com>
2020-06-10 11:29:51 -0400

    coded the reducers switch statement

--- src/reducers/accountReducer.js
+++ src/reducers/accountReducer.js
@@ -1,3 +1,9 @@
 export default function accountReducer(state = {accounts: []}, action) {
+
+  switch (action.type) {
+    case "FETCH_ACCOUNTS":
+    return {accounts: action.payload};
+      default:
      return state
+   }
 }
