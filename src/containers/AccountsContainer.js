9e761a2d1210bf8ecbf874e6c094accf95417e37
github username <levik010@gmail.com>
2020-06-10 11:29:02 -0400

    added mapStateToProps, sent accounts down to lists component through props

---
+++ src/containers/AccountsContainer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import AccountForm from '../components/AccountForm'
+import AccountsList from '../components/AccountsList'
+import {connect} from 'react-redux'
+import fetchAccounts from '../actions/fetchAccounts'
+
+
+class  AccountsContainer extends React.Component {
+
+  componentDidMount() {
+
+    this.props.fetchAccounts()
+
+  }
+
+  render() {
+    return (
+
+      <div>
+        <AccountForm />
+        <AccountsList accounts={this.props.accounts}/>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return  {
+    accounts: state.accounts
+  }
+
+}
+
+export default connect(mapStateToProps, {fetchAccounts})(AccountsContainer)
